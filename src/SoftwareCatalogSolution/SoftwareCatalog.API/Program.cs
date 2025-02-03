985107ff3f802f35b392d6eb88cb2ed43f249ab8
Andrew Le <drew@drewle.com>
2025-02-03 14:53:56 -0500

    Initial

---
+++ src/SoftwareCatalogSolution/SoftwareCatalog.API/Program.cs
@@ -0,0 +1,24 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+// Above this line is the internals of our api project
+var app = builder.Build(); // THE LINE IN THE SAND
+// Below this line is configuring how the web server handles incoming requests/responses
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run(); // a blocking infinite for loop.
