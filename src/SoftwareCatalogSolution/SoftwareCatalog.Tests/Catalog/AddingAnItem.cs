6da026e827fac47a28ec440f98c663ba14351f0a
Jeffry Gonzalez <jeff@hypertheory.com>
2025-02-05 10:50:03 -0500

    before break

--- src/SoftwareCatalogSolution/SoftwareCatalog.Tests/Catalog/AddingAnItem.cs
+++ src/SoftwareCatalogSolution/SoftwareCatalog.Tests/Catalog/AddingAnItem.cs
@@ -5,20 +5,22 @@ using SoftwareCatalog.Api.Catalog;
 namespace SoftwareCatalog.Tests.Catalog;
 public class AddingAnItem
 {
-    [Fact]
-    public async Task CanAddAnItem()
+    [Theory]
+    [InlineData("Visual Studio Code", "microsoft", CatalogItemLicenceTypes.OpenSource)]
+    [InlineData("Visual Studio", "microsoft", CatalogItemLicenceTypes.Paid)]
+    public async Task CanAddAnItem(string name, string vendor, CatalogItemLicenceTypes license)
     {
         var host = await AlbaHost.For<Program>();
 
 
         var itemToPost = new CatalogItemRequestModel
         {
-            Name = "Visual Studio Code"
+            Name = name
         };
-
+        var resource = $"/vendors/{vendor.ToLower()}/{license.ToString().ToLower()}";
         var response = await host.Scenario(api =>
         {
-            api.Post.Json(itemToPost).ToUrl("/vendors/microsoft/opensource");
+            api.Post.Json(itemToPost).ToUrl(resource);
             api.StatusCodeShouldBe(201);
         });
 
