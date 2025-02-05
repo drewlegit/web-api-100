e7bdfc8b77809fd9aad54e9b44cf76c15de54b34
Jeffry Gonzalez <jeff@hypertheory.com>
2025-02-05 11:24:28 -0500

    after refactor

--- src/SoftwareCatalogSolution/SoftwareCatalog.Api/Catalog/Endpoints/AddingACatalogItem.cs
+++ src/SoftwareCatalogSolution/SoftwareCatalog.Api/Catalog/Endpoints/AddingACatalogItem.cs
@@ -10,44 +10,21 @@ public class AddingACatalogItem(IDocumentSession session) : ControllerBase
 
     [HttpPost("/vendors/microsoft/opensource")]
     public async Task<ActionResult> AddOpenSourceItemToCatalogAsync(
-        [FromBody] CatalogItemRequestModel request)
-    {
+        [FromBody] CatalogItemRequestModel request) => await AddCatalogItem(request, CatalogItemLicenceTypes.OpenSource);
 
-        // TODO: 1. Do Some validation
-        // TODO : 2 save it to the database.
-        var entityToSave = new CatalogItemEntity
-        {
-            Id = Guid.NewGuid(),
-            Licence = CatalogItemLicenceTypes.OpenSource,
-            Name = request.Name,
-            Vendor = "Microsoft"
-        };
 
-        session.Store(entityToSave);
-        await session.SaveChangesAsync(); // Do the actual work and save into the database
-
-        // Step 3: send them the response
-        // fake this for right now
-        var fakeResponse = new CatalogItemResponseDetailsModel
-        {
-            Id = entityToSave.Id,
-            Licence = entityToSave.Licence,
-            Name = entityToSave.Name,
-            Vendor = entityToSave.Vendor
-        };
-        return StatusCode(201, fakeResponse);
-    }
     [HttpPost("/vendors/microsoft/paid")]
-    public async Task<ActionResult> AddPaidItemToCatalogAsync(
-       [FromBody] CatalogItemRequestModel request)
-    {
+    public async Task<ActionResult> AddPaidSourceItemToCatalogAsync(
+        [FromBody] CatalogItemRequestModel request) => await AddCatalogItem(request, CatalogItemLicenceTypes.Paid);
 
+    private async Task<ActionResult> AddCatalogItem(CatalogItemRequestModel request, CatalogItemLicenceTypes license)
+    {
         // TODO: 1. Do Some validation
         // TODO : 2 save it to the database.
         var entityToSave = new CatalogItemEntity
         {
             Id = Guid.NewGuid(),
-            Licence = CatalogItemLicenceTypes.Paid,
+            Licence = license,
             Name = request.Name,
             Vendor = "Microsoft"
         };
