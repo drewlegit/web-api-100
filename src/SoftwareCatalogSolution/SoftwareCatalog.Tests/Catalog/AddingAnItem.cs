aa960101c837c18d0ed1d4a264c7e1fff507ea9c
Jeffry Gonzalez <jeff@hypertheory.com>
2025-02-05 12:18:34 -0500

    fixtures in xunit

--- src/SoftwareCatalogSolution/SoftwareCatalog.Tests/Catalog/AddingAnItem.cs
+++ src/SoftwareCatalogSolution/SoftwareCatalog.Tests/Catalog/AddingAnItem.cs
@@ -1,16 +1,15 @@
-﻿
-using Alba;
-using SoftwareCatalog.Api.Catalog;
+﻿using SoftwareCatalog.Api.Catalog;
 
 namespace SoftwareCatalog.Tests.Catalog;
-public class AddingAnItem
+public class AddingAnItem(CatalogSystemTestFixture fixture) : IClassFixture<CatalogSystemTestFixture>
 {
+
     [Theory]
     [InlineData("Visual Studio Code", "microsoft", CatalogItemLicenceTypes.OpenSource)]
     [InlineData("Visual Studio", "microsoft", CatalogItemLicenceTypes.Paid)]
     public async Task CanAddAnItem(string name, string vendor, CatalogItemLicenceTypes license)
     {
-        var host = await AlbaHost.For<Program>();
+
 
 
         var itemToPost = new CatalogItemRequestModel
@@ -18,7 +17,7 @@ public class AddingAnItem
             Name = name
         };
         var resource = $"/vendors/{vendor.ToLower()}/{license.ToString().ToLower()}";
-        var response = await host.Scenario(api =>
+        var response = await fixture.Host.Scenario(api =>
         {
             api.Post.Json(itemToPost).ToUrl(resource);
             api.StatusCodeShouldBe(201);
@@ -29,7 +28,7 @@ public class AddingAnItem
 
         //   Assert.Equal(expectedResponse, body);
 
-        var getResponse = await host.Scenario(api =>
+        var getResponse = await fixture.Host.Scenario(api =>
         {
             api.Get.Url($"/catalog/{responseFromThePost.Id}");
         });
@@ -40,4 +39,6 @@ public class AddingAnItem
         Assert.Equal(responseFromThePost, responseFromGet);
 
     }
+
+
 }
